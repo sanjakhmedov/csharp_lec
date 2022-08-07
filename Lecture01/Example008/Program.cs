4b14cddae3acafbb0ba5f051c9baa36dfa27fb3c
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-08 00:43:22 +0500

    re-set up

---
+++ Lecture01/Example008/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int amount = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        amount = distance/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        amount = distance/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * amount;
+    count++;
+}
+
+Console.WriteLine(count);
