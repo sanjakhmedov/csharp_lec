f07d036148f78590b3d2f60b2f874f355bd1ac3f
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-07 18:30:25 +0500

    added new folder

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please enter your name!");
+String username = Console.ReadLine();
+
+if (username == "Masha") 
+{
+    Console.WriteLine($"Hello DEAR {username}");
+} 
+else 
+{
+    Console.WriteLine($"Hello {username}");
+}
