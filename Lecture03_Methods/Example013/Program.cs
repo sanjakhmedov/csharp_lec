dae3570bb0b425cc559880b7a96b4b1b6f972996
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-15 18:01:34 +0500

    example 13

--- Lecture03_Methods/Example013/Program.cs
+++ Lecture03_Methods/Example013/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Substitute "k" to "K", spaces to "-", "C" to "c".
+
+string text = "It is a long established fact that a reader will be "
+            + "distracted by the readable content of a page when "
+            + "looking at its layout.";
+
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, 'r', 'R');
+Console.WriteLine(newText);
