b472f1153da8a369fae80b38c36b9ac96d316a30
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-15 15:17:55 +0500

    lec 2

--- Lecture02/Example001/Program.cs
+++ Lecture02/Example001/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+int n = arr.Length;
+
+int index = 0;
+int find = 7;
+
+while (index < n)
+{
+    if (arr[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
+Console.WriteLine(n);
--- Lecture02/Example002/Program.cs
+++ Lecture02/Example002/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    
+}
+
+
+
+
+
+int[] array = new int[10];
