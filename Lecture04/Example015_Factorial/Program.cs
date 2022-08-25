8d7294ba49adce3c8a14a677ac3f44dc0e6b9b91
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-25 13:16:44 +0500

    lec 4 folder added

---
+++ Lecture04/Example015_Factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿double Factorial(double n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (double i = 1; i < 30; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
